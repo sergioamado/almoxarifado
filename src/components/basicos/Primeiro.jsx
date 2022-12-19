04ac90249dc26736533dd421dd3059bba640624b
sergioamado <sergio.santos@dcomp.ufs.br>
2022-12-19 11:37:36 -0300

    até a aula 16

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Primeiro(){
+    const msg = 'Sejam bem vindos ao react'
+    return ( 
+    <div>
+        <h2>Primeiro Componente com react</h2>
+        <p>{msg}</p>
+    </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import './index.css'
+import './index.css'  
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import Primeiro from './components/basicos/Primeiro.jsx'
+import ComParametro from './components/basicos/ComParametro'
 
-const tag =  <strong>Olá React!!!</strong>
 
 ReactDOM.render (
                 <div>
-                    {tag}
-                        </div>, 
+                    <Primeiro></Primeiro>
+                    <ComParametro titulo= "Situação do aluno" 
+                                  aluno= "Anderson" 
+                                  nota= {9.3}/>
+                    <ComParametro titulo= "Situação do aluno" 
+                                  aluno= "Sergio" 
+                                  nota= {9.9}/>
+                </div>, 
                 document.getElementById('root')
                 )
