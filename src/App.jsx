20686169f141a76811b50dd605867fba3c1d8be3
sergioamado <sergio.santos@dcomp.ufs.br>
2023-01-11 09:02:13 -0300

    Aula 25

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from "react";
 
 import Card from "./components/layout/Card";
@@ -8,30 +9,24 @@ import Primeiro from "./components/basicos/Primeiro";
 
 export default function App(props) {
   return (
-    <div id="app">
+    <div className="App">
       <h1> Fundamentos React </h1>
 
-      <Card titulo="#04 - Desafio Aleatorio">
-        <Aleatorio min={1} max={60} />
-        
-      </Card>
-      <Card titulo="#03 - Fragmento">
-      <Fragmento />
-        
-      </Card>
-      <Card titulo="#02 - ComParametro">
-        
-      <ComParametro titulo="Situação do aluno" aluno="Anderson" nota={9.3} />
-      <ComParametro titulo="Situação do aluno" aluno="Sergio" nota={9.9} />
-        
-      </Card>
-      <Card titulo="#01 - Primeiro">
-        
-      <Primeiro />
-        
-      </Card>      
-
-
+      <div className="Cards">
+        <Card titulo="#04 - Desafio Aleatorio" color="080">
+          <Aleatorio min={1} max={60} />
+        </Card>
+        <Card titulo="#03 - Fragmento">
+          <Fragmento />
+        </Card>
+        <Card titulo="#02 - ComParametro">
+          <ComParametro titulo="Situação do aluno" aluno="Anderson" nota={9.3} />
+          <ComParametro titulo="Situação do aluno" aluno="Sergio" nota={9.9} />
+        </Card>
+        <Card titulo="#01 - Primeiro">
+          <Primeiro />
+        </Card>
+      </div>
     </div>
   );
 }
