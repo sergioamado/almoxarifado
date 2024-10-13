48c3fe8dfb80b5726bcfe2459904cc2185b2f5d9
Sergio Amado <sergio.santos@dcomp.ufs.br>
2024-10-13 11:46:32 -0300

    :bricks: :build: Api completa favor testar

--- src/main/java/com/example/almoxarifado/service/ItensService.java
+++ src/main/java/com/example/almoxarifado/service/ItensService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ItensService {
+public class ItensService  {
 
     @Autowired
     private ItensRepository itensRepository;
